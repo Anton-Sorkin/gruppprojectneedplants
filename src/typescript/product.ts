c3ec4d2de1e666cf348e2d513d272c10aa84bca8
AZAFARANI <zafaranian8@gmail.com>
2021-12-07 13:05:55 +0100

    ?

--- src/typescript/product.ts
+++ src/typescript/product.ts
@@ -1,3 +1,30 @@
+
+import { Product } from "./models/Product";
+
 window.onload = function () {
-	/*Write normal typescript*/
-};
+
+	let theobjectfromstorage: string =  sessionStorage.getItem("productToProductPage");
+	
+	let storageasobject: Product = JSON.parse(theobjectfromstorage);
+
+	console.log(storageasobject);
+
+	
+	let createimage = document.getElementById("image") as HTMLImageElement;
+	createimage.src = storageasobject.image;
+	createimage.alt = " image of product ";
+
+	let createtitle = document.getElementById("product-h1") as HTMLHeadingElement;
+	createtitle.innerHTML = storageasobject.name;
+
+	let createdescription = document.getElementById("product-description") as HTMLParagraphElement;
+	createdescription.innerHTML = storageasobject.description;
+
+	let createprice = document.getElementById("product-price-2") as HTMLSpanElement;
+	
+	createprice.innerHTML = storageasobject.price.toString();
+
+	
+    
+}
+
