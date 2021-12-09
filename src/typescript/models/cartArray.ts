69f9db0851be8165344bc92fb63037aebb508a0e
Anton-Sorkin <anton.sorkin98@outlook.com>
2021-12-09 10:02:54 +0100

    parcel

--- src/typescript/models/cartArray.ts
+++ src/typescript/models/cartArray.ts
@@ -1,3 +1,6 @@
 import { CartProduct } from "./CartClass";
 
-let cartArray: CartProduct[] = [];
+let cartArray: CartProduct[] = [
+	{ name: "Eucalyptus", image: "../../assets.Eucalyptus_cropped.jpg", price: 10, amount: 0 },
+	{ name: "Euca", image: "../../assets.Eucalyptus_cropped.jpg", price: 15, amount: 0 },
+];
