6ac302f1d5393931527fdac88f6af04d2d0846af
LudvigThunberg <ludvigthunberg@gmail.com>
2021-12-09 15:39:05 +0100

    started tests

---
+++ cypress/integration/shopHome.spec.ts
@@ -0,0 +1,18 @@
+describe("Web shop home page tests", () => {
+  it("should create html from productArray", () => {
+    cy.visit("http://localhost:1234");
+    cy.get(".home-single-product-wrapper:first .home-product-name").should(
+      "have.html",
+      "Eucalyptus"
+    );
+    cy.get(".home-single-product-wrapper:first .home-product-price").should(
+      "have.html",
+      "10:-"
+    );
+  });
+  it("should add products to cart at click", () => {
+    cy.visit("http://localhost:1234");
+    cy.get("#home-cart-button").click();
+    cy.url().should("should.include", "http://localhost:1234/cart.html");
+  });
+});
