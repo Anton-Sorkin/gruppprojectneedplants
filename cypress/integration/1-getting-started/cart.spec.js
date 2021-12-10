cd34be1bbc2cc1925018bd7c3eec89c9842bf635
Anton-Sorkin <anton.sorkin98@outlook.com>
2021-12-10 10:28:36 +0100

    parcel and cart

--- cypress/integration/1-getting-started/cart.spec.js
+++ cypress/integration/1-getting-started/cart.spec.js
@@ -1,11 +1,11 @@
-describe("Button testing", () => {
-	it("Should go to home", () => {
-		cy.visit("http://localhost:1234");
-		cy.get(".home-button").click();
-	});
+// describe("Button testing", () => {
+// 	it("Should go to home", () => {
+// 		cy.visit("http://localhost:1234");
+// 		cy.get(".home-button").click();
+// 	});
 
-	it("Should go to checkout", () => {
-		cy.visit("http://localhost:1234");
-		cy.get(".cart-makeorder").click();
-	});
-});
+// 	it("Should go to checkout", () => {
+// 		cy.visit("http://localhost:1234");
+// 		cy.get(".cart-makeorder").click();
+// 	});
+// });
--- src/typescript/models/cartArray.ts
+++ src/typescript/models/cartArray.ts
@@ -1,6 +1,7 @@
 import { CartProduct } from "./CartClass";
 
-let cartArray: CartProduct[] = [
-	{ name: "Eucalyptus", image: "../../assets.Eucalyptus_cropped.jpg", price: 10, amount: 0 },
-	{ name: "Euca", image: "../../assets.Eucalyptus_cropped.jpg", price: 15, amount: 0 },
+export let cartArray: CartProduct[] = [
+	{ name: "Eucalyptus", image: "../../assets.Eucalyptus_cropped.jpg", price: 10, amount: 1 },
+
+	{ name: "Euca", image: "../../assets.Eucalyptus_cropped.jpg", price: 15, amount: 1 },
 ];
