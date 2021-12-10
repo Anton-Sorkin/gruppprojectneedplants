6912074e4449b94a53b3035ecef348e5ac0eace2
LudvigThunberg <ludvigthunberg@gmail.com>
2021-12-10 11:19:01 +0100

    Fixed parcel problem

--- dist/cart.463f777c.js
+++
@@ -1,4 +0,0 @@
-window.onload = function() {
-/*Write normal typescript*/ };
-
-//# sourceMappingURL=cart.463f777c.js.map
--- dist/checkout.f7f06f80.js
+++
@@ -1,4 +0,0 @@
-window.onload = function() {
-/*Write normal typescript*/ };
-
-//# sourceMappingURL=checkout.f7f06f80.js.map
