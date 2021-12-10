a13660d8e18acafa04445b6a2ba923db3afcc68e
LudvigThunberg <ludvigthunberg@gmail.com>
2021-12-10 11:28:03 +0100

    Parcel fix

--- src/typescript/models/cartArray.ts
+++ src/typescript/models/cartArray.ts
@@ -1,14 +1,8 @@
 /* import { CartProduct } from "./CartClass";
 
-<<<<<<< HEAD
-let cartArray: CartProduct[] = [
-	{ name: "Eucalyptus", image: "../../assets.Eucalyptus_cropped.jpg", price: 10, amount: 0 },
-	{ name: "Euca", image: "../../assets.Eucalyptus_cropped.jpg", price: 15, amount: 0 },
-];*/
-=======
 export let cartArray: CartProduct[] = [
 	{ name: "Eucalyptus", image: "../../assets.Eucalyptus_cropped.jpg", price: 10, amount: 1 },
 
 	{ name: "Euca", image: "../../assets.Eucalyptus_cropped.jpg", price: 15, amount: 1 },
 ];
->>>>>>> feature/totalCart
+*/
