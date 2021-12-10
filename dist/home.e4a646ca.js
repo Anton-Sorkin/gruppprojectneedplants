0b532fb4316740b6c2b679bbdbbf7dbec24ca11c
LudvigThunberg <ludvigthunberg@gmail.com>
2021-12-10 08:43:37 +0100

    parcel commit

--- dist/home.e4a646ca.js
+++ dist/home.e4a646ca.js
@@ -540,22 +540,22 @@ parcelHelpers.defineInteropFlag(exports);
 parcelHelpers.export(exports, "productArray", ()=>productArray
 );
 var _product = require("./Product");
-let eucalyptus = new _product.Product("Eucalyptus", "../../assets.Eucalyptus_cropped.jpg", "Upgrade your life with a wondeful twig of eucalyptus. Great for your home office and for your living room.", 10, 0);
-let peaceLily = new _product.Product("Peace Lily", "", "The Pice Lily sybolizes peace, sympathy, prosparety and innocence. This plant will brighten upp your darkest corners.", 10, 0);
-let monstera = new _product.Product("Monstera", "", "The extravagant Monstera is greatly poular and rightfully so, if taken care of correctly it can get up to 10 meters high and the leaves are big as a giants hand", 15, 0);
-let porcelainFlower = new _product.Product("Porcelain Flower", "", "The leaves are thick and the smell of the bold, beautiful flowers will remind you of grandma, in her glory days.", 15, 0);
-let peony = new _product.Product("Peony", "", "This ball off deliciousness comes from the countries who’s beaches you really wanna visit. You’ll love it!", 10, 0);
-let lily = new _product.Product("Lily", "", "Since 1753 when Carl von Linné braught this beauty to Europe, People have loved and chairiched the magestic Lily!", 15, 0);
-let tulip = new _product.Product("Tulip", "", "This dutch majesty grows in the spring time. when the sun is warm and you’re full of love you’ll know it’s time to buy some tulips.", 10, 0);
-let hortensia = new _product.Product("Hortensia", "", "This beauty is easy to grow in your garden and magificent on your dining table. Buy it now, thank us later!", 15, 0);
-let mixedCacti = new _product.Product("Mixed Cacti", "", "This party of cacti will keep you company through the darkest nights and the brightest days. It’s social skills are uncanny.", 20, 0);
-let miniatureCactus = new _product.Product("Miniature Cactus", "", "This unfriendly little bastard is both beautifuland disturbing, your eyes will love it, your fingers won’t.", 10, 0);
-let aloe = new _product.Product("Aloe", "", "This guy will heal both your wounds and your soul. On top of that it’s great if you lack a green thumb.", 10, 0);
-let fingerCactus = new _product.Product("Finger Cactus", "", "Do you love fishing? If that’s the case, this is the plant for you. Focus on fishing gear beacuse this cactus will survive during your long fishing trips.", 20, 0);
-let turquoisePartyPot = new _product.Product("Turquoise Party Pot", "", "This turquois flowerpot is the life of a party, a must to any serious collection of flower pots.", 15, 0);
-let ruggedCeramicPot = new _product.Product("Rugged Ceramic Pot", "", "A rugged ceramic pot, great for flowers and great for soup. Get your groove on and get your plant in one of thies fine things.", 20, 0);
-let whitePlasticPot = new _product.Product("White Plastic Pot", "", "White, plastic and romantic. Stick your favorite plant in one of these and you’ll never walk alone.", 20, 0);
-let plainWhiteCeramicPot = new _product.Product("Plain White Ceramic Pot", "", "The Plain White Ceramic Pot is simplicity in its purest form, great for any type of interior design.", 15, 0);
+let eucalyptus = new _product.Product("Eucalyptus", "../../assets.Eucalyptus_cropped.jpg", "Upgrade your life with a wondeful twig of eucalyptus. Great for your home office and for your living room.", 10, 1);
+let peaceLily = new _product.Product("Peace Lily", "", "The Pice Lily sybolizes peace, sympathy, prosparety and innocence. This plant will brighten upp your darkest corners.", 10, 1);
+let monstera = new _product.Product("Monstera", "", "The extravagant Monstera is greatly poular and rightfully so, if taken care of correctly it can get up to 10 meters high and the leaves are big as a giants hand", 15, 1);
+let porcelainFlower = new _product.Product("Porcelain Flower", "", "The leaves are thick and the smell of the bold, beautiful flowers will remind you of grandma, in her glory days.", 15, 1);
+let peony = new _product.Product("Peony", "", "This ball off deliciousness comes from the countries who’s beaches you really wanna visit. You’ll love it!", 10, 1);
+let lily = new _product.Product("Lily", "", "Since 1753 when Carl von Linné braught this beauty to Europe, People have loved and chairiched the magestic Lily!", 15, 1);
+let tulip = new _product.Product("Tulip", "", "This dutch majesty grows in the spring time. when the sun is warm and you’re full of love you’ll know it’s time to buy some tulips.", 10, 1);
+let hortensia = new _product.Product("Hortensia", "", "This beauty is easy to grow in your garden and magificent on your dining table. Buy it now, thank us later!", 15, 1);
+let mixedCacti = new _product.Product("Mixed Cacti", "", "This party of cacti will keep you company through the darkest nights and the brightest days. It’s social skills are uncanny.", 20, 1);
+let miniatureCactus = new _product.Product("Miniature Cactus", "", "This unfriendly little bastard is both beautifuland disturbing, your eyes will love it, your fingers won’t.", 10, 1);
+let aloe = new _product.Product("Aloe", "", "This guy will heal both your wounds and your soul. On top of that it’s great if you lack a green thumb.", 10, 1);
+let fingerCactus = new _product.Product("Finger Cactus", "", "Do you love fishing? If that’s the case, this is the plant for you. Focus on fishing gear beacuse this cactus will survive during your long fishing trips.", 20, 1);
+let turquoisePartyPot = new _product.Product("Turquoise Party Pot", "", "This turquois flowerpot is the life of a party, a must to any serious collection of flower pots.", 15, 1);
+let ruggedCeramicPot = new _product.Product("Rugged Ceramic Pot", "", "A rugged ceramic pot, great for flowers and great for soup. Get your groove on and get your plant in one of thies fine things.", 20, 1);
+let whitePlasticPot = new _product.Product("White Plastic Pot", "", "White, plastic and romantic. Stick your favorite plant in one of these and you’ll never walk alone.", 20, 1);
+let plainWhiteCeramicPot = new _product.Product("Plain White Ceramic Pot", "", "The Plain White Ceramic Pot is simplicity in its purest form, great for any type of interior design.", 15, 1);
 let productArray = [
     eucalyptus,
     peaceLily,
