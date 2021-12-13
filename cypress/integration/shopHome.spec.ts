34ac0101918e2c07d408de642a8e2d0df16af37e
LudvigThunberg <ludvigthunberg@gmail.com>
2021-12-13 19:59:39 +0100

    Added test for home

--- cypress/integration/shopHome.spec.ts
+++ cypress/integration/shopHome.spec.ts
@@ -1,6 +1,6 @@
 describe("Web shop home page tests", () => {
   it("should create html from productArray", () => {
-    cy.visit("http://localhost:1234");
+    cy.visit("http://localhost:1234/html/home.html");
     cy.get(".home-single-product-wrapper:first .home-product-name").should(
       "have.html",
       "Eucalyptus"
@@ -10,9 +10,34 @@ describe("Web shop home page tests", () => {
       "10:-"
     );
   });
-  it("should add products to cart at click", () => {
-    cy.visit("http://localhost:1234");
+
+  it("should go to cart when click on cart", () => {
+    cy.visit("http://localhost:1234/html/home.html");
     cy.get("#home-cart-button").click();
-    cy.url().should("should.include", "cart.html");
+    cy.url().should("include", "/html/cart.html");
+  });
+
+  it("should add product to cart click add to cart", () => {
+    cy.visit("http://localhost:1234/html/home.html");
+    cy.get(
+      "#home-all-products-wrapper .home-single-product-wrapper:first .home-product-cart-link"
+    ).click();
+    cy.get("#cart-counter").should("have.class", "visible");
+  });
+
+  it("should go to product page when clicking on product", () => {
+    cy.visit("http://localhost:1234/html/home.html");
+    cy.get(
+      "#home-all-products-wrapper .home-single-product-wrapper:first .home-image-wrapper"
+    ).click();
+    cy.url().should("include", "/html/product.html");
+  });
+
+  it("should show image on page", () => {
+    cy.visit("http://localhost:1234/html/home.html");
+    cy.get(
+      "#home-all-products-wrapper .home-single-product-wrapper:first .home-image-wrapper > img"
+    );
+    cy.should("have.attr", "src", "../assets/Eucalyptus_cropped.jpg");
   });
 });
